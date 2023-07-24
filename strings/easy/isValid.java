38fcd7b1895877f927f28003093603da040c3e9e
Nandam Sohan Srikanth <92619014+SohanSrikanth@users.noreply.github.com>
2023-07-24 23:39:20 +0530

    Update 1 24-7-23

---
+++ strings/easy/isValid.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public boolean isValid(String s) {
+        char[] carr = s.toCharArray();
+        Stack<Character> st = new Stack<>();
+        for (char c : carr) {
+            if (c == '(' || c == '[' || c == '{') {
+                st.push(c);
+            } else {
+                if (st.isEmpty()) {
+                    return false;
+                } else if (c == ')' && st.peek() == '(' || c == '}' && st.peek() == '{'
+                        || c == ']' && st.peek() == '[') {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
